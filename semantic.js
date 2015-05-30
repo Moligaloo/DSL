9b1ff456ecbadde04f93b90ea59bd89cfe381108
Moligaloo <moligaloo@gmail.com>
2015-05-30 12:10:14 +0800

    add semantic.js and remove null and undefined elements of arrays

---
+++ semantic.js
@@ -0,0 +1,30 @@
+
+var SemanticParser = function(){
+
+}
+
+SemanticParser.prototype.parse = function(raw) {
+	return this.removeNullElements(raw);
+}
+
+SemanticParser.prototype.removeNullElements = function(raw) {
+	if($.isArray(raw)){
+		var cleaned = new Array();
+		for(var i=0; i<raw.length; i++){
+			var elem = raw[i];
+			if(elem == null || elem == undefined)
+				continue;
+			else
+				cleaned.push(this.removeNullElements(elem));
+		}
+
+		return cleaned;
+	}else if(raw instanceof Object){
+		for(var name in raw){
+			raw[name] = this.removeNullElements(raw[name]);
+		}
+		return raw;
+	}else{
+		return raw;
+	}
+};
